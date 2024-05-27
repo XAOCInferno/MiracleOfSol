be892e5608cc5bd2a38b44f0a26f64bffa0bd7a5
BenB <TestXBot@gmail.com>
2024-05-27 18:38:47 +0100

    Comments on fog code

--- MiracleOfSol_GameProject/Assets/NewEngine/Fog Of War/FogOfWarController.cs
+++ MiracleOfSol_GameProject/Assets/NewEngine/Fog Of War/FogOfWarController.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.HighDefinition;
-using static UnityEngine.EventSystems.EventTrigger;
 
 public class FogOfWarController : MonoBehaviour
 {
@@ -123,11 +122,55 @@ public class FogOfWarController : MonoBehaviour
         int Radius = VisionInfo.Radius;
         double rad2 = System.Math.Pow(Radius, 2);
 
+        //Calculate top and left of the circle
+        int left = Centre.x - Radius;
+        int top = Centre.y - Radius;
+
+        //Iterate over each point in the range of far left/top to centre
+        for (int x = left; x <= Centre.x; x++)
+        {
+            for (int y = top; y <= Centre.y; y++)
+            {
+
+                //Calculate left and top 1 whole circle away
+                int farLeft = x - Centre.x;
+                int farTop = y - Centre.y;
+
+                //x2 + y2 <= r2 (in circle) [Pythagorus] 
+                if (System.Math.Pow(farLeft, 2) + System.Math.Pow(farTop, 2) <= rad2)
+                {
+                    int xFlipped = Centre.x - farLeft;
+                    int yFlipped = Centre.y - farTop;
+
+                    //Convert points into positions in the byte array
+                    Vector2Int CoordXY = PointToBytePosition(new(x, y));
+                    Vector2Int CoordXYFlipped = PointToBytePosition(new(xFlipped, yFlipped));
+                    Vector2Int CoordXFlippedYFlipped = PointToBytePosition(new(x, yFlipped));
+                    Vector2Int CoordXFlippedY = PointToBytePosition(new(xFlipped, y));
+
+                    //Set the positions in array as false (visible area)
+                    FogAsByteArray[CoordXY.x, CoordXY.y] = 0;
+                    FogAsByteArray[CoordXYFlipped.x, CoordXYFlipped.y] = 0;
+                    FogAsByteArray[CoordXFlippedYFlipped.x, CoordXFlippedYFlipped.y] = 0;
+                    FogAsByteArray[CoordXFlippedY.x, CoordXFlippedY.y] = 0;
+                    
+                }
+            }
+        }
+    }
+
+    private void DrawSquareCutout(CircularVisionEmitter VisionInfo)
+    {
+
+        Vector2Int Centre = VisionInfo.Centre;
+        int Radius = VisionInfo.Radius;
+        double rad2 = System.Math.Pow(Radius, 2);
+
         for (int x = Centre.x - Radius; x <= Centre.x; x++)
         {
             for (int y = Centre.y - Radius; y <= Centre.y; y++)
             {
-                if (System.Math.Pow(x - Centre.x, 2) + System.Math.Pow(y - Centre.y, 2) <= rad2)
+                if (x - Centre.x + y - Centre.y <= Radius)
                 {
                     int xSym = Centre.x - (x - Centre.x);
                     int ySym = Centre.y - (y - Centre.y);
@@ -141,7 +184,7 @@ public class FogOfWarController : MonoBehaviour
                     FogAsByteArray[CoordXYSym.x, CoordXYSym.y] = 0;
                     FogAsByteArray[CoordXSymYSym.x, CoordXSymYSym.y] = 0;
                     FogAsByteArray[CoordXSymY.x, CoordXSymY.y] = 0;
-                    
+
                 }
             }
         }
--- MiracleOfSol_GameProject/Assets/NewEngine/Fog Of War/HideInFog.cs
+++ MiracleOfSol_GameProject/Assets/NewEngine/Fog Of War/HideInFog.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.VFX;
 
 public class HideInFog : MonoBehaviour
 {
